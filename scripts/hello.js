601252dfd602bd12401be18e0029f3697b19af67
mbadgett <m_badgett@hotmail.com>
2019-08-20 22:12:33 -0700

    Updates! we have callbacks

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,4 +1,39 @@
 
+class NameClass {
+    constructor() {
+        this.name="World";
+    }
+}
+
+var nameObject = new NameClass();
+
 function helloWorld() {
-    alert("Hello World");
+    alert("Hello " + nameObject.name);
+}
+
+function letsGetSomeDataAsyncronously() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("POST", "demo_post.asp", true);
+    xhttp.send();
+}
+
+var onLoadCallBack = () => {
+    var submitButton = document.getElementById("submitName");
+    var submitButtonCallback = () => {
+        var textbox = document.getElementById("nameBox");
+        nameObject.name = textbox.value;
+        textbox.value = "";
+    };
+    submitButton.addEventListener("click", submitButtonCallback)
+}
+
+window.onload = onLoadCallBack;
+
+function doSomething() {
+    // Make some rest api call or do some math or something.
 }
